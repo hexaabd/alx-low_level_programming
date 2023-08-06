2d1cec9b678ca181dbcd04d963152a61c6598628
achraf abdeladim <dark@ash>
2023-08-06 08:08:09 -1000

    push

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+at this folder we should be write or made like a makfile a script who can creat a static library called liball.a from all the .c files 
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,6 @@
+#include "main.h"
+#include<stdio.h>
+int main(void)
+{
+	printf("every moment evry second till the angel of death comming i will try my best to reach my dreams");
+}
